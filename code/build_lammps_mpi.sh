190dc955d95a04a11ea761bc9d216d50f985e7d8
Petra Papež <134068856+PPapez@users.noreply.github.com>
2025-11-14 09:24:12 +0100

    Update build script to remove debug options
    Removed debug build type and export compile commands.

--- code/build_lammps_mpi.sh
+++ code/build_lammps_mpi.sh
@@ -14,8 +14,6 @@ cmake ../cmake \
 -D PKG_EXTRA-COMPUTE=on \
 -D PKG_EXTRA-MOLECULE=on \
 -D BUILD_MPI=yes \
--D CMAKE_BUILD_TYPE=Debug \
--D CMAKE_EXPORT_COMPILE_COMMANDS=on \
 -D LAMMPS_MACHINE=mpi
 
 make -j 12
